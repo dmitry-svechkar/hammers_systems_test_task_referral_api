f0b19ab20227d669093bd1b90694f71f95025d61
Дмитрий Свечкарь <svechkar.dmitry@yandex.ru>
2024-04-21 19:40:04 +0300

    admin panel settings

--- ref_system/ref_system/urls.py
+++ ref_system/ref_system/urls.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
 from django.urls import include, path
+from rest_framework.authtoken.models import TokenProxy
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
 ]
+
+
+admin.site.site_header = 'Администрирование площадки'
+admin.site.site_title = 'Администрирование площадки'
+admin.site.index_title = 'Добро пожалователь в админ-панель'
+
+admin.site.unregister(Group)
+admin.site.unregister(TokenProxy)
--- ref_system/users/admin.py
+++ ref_system/users/admin.py
@@ -1,7 +1,21 @@
 from django.contrib import admin
+
 from users.models import CustomUserModel
 
 
 @admin.register(CustomUserModel)
 class UserAdmin(admin.ModelAdmin):
-    pass
+    """ Настройка отображения админ панели. """
+    list_display = (
+        'reg_data',
+        'telephone_number',
+        'is_active',
+        'user_referal_code',
+        'invitation_code'
+    )
+    search_fields = ('telephone_number',)
+    ordering = ('-reg_data',)
+    fields = [
+        'telephone_number', 'is_active',
+        ('user_referal_code', 'invitation_code')
+    ]
